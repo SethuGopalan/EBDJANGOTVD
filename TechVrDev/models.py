7f2fbf196a543aa3d25f1a918b9c523f99820963
Sethu Gopalan <Sethu.Gopalan@cytel.com>
2021-06-08 15:39:35 -0400

    TechVrDevProject models created

--- TechVrDev/models.py
+++ TechVrDev/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class TechVrDev(models.Model):
+    title = models.CharField(max_length=240)
+    description =models.TextField()
+    technology = models.CharField(max_length=100)
+    image= models.FilePathField(path="/img")
+
+
--- ebdjangoTVD/settings.py
+++ ebdjangoTVD/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ebdjangoTVD.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['EBDJANGOTVD/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
