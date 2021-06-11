17921a64b81297ed2c6c16f2ba4ec3d78f843d65
Sethu Gopalan <Sethu.Gopalan@cytel.com>
2021-06-11 10:23:45 -0400

    intex and detail template created

--- TechVrDev/urls.py
+++ TechVrDev/urls.py
@@ -3,4 +3,5 @@ from TechVrDev import views
 
 urlpatterns = [
     path("", views.TechVrDev, name="TechVrDev"),
+    path("<int:pk>/", views.TechVrDev_detail, name="TechVrDev_detail"),
 ]
--- TechVrDev/views.py
+++ TechVrDev/views.py
@@ -1,6 +1,21 @@
+from TechVrDev.models import TechVrDevapp
 from django.shortcuts import render
-
+from TechVrDev.models import TechVrDevapp
 
 
 def TechVrDev(request):
     return render(request, 'techVrDev.html', {})
+
+def TechVrDev_intex(request):
+    TechVrDev = TechVrDevapp.objects.all()
+    context = {
+        'TechVrDev': TechVrDev
+    }
+    return render(request,'TechVrDev_intex',context)
+
+def TechVrDev_detail(request, pk):
+    TechVrDev = TechVrDevapp.objects.get(pk= pk)
+    context = {
+        'TechVrDevapp' : TechVrDevapp
+    }
+    return render(request,'TechVrDev_detail.html',context)
